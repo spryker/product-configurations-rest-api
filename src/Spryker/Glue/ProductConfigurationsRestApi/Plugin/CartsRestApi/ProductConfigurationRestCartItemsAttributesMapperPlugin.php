b842561139ccf854edf86f4ad8e5f9ee9ab46570
Andrey Tkachenko <andrey.tkachenko@spryker.com>
2023-08-17 17:38:44 +0200

    Update ProductConfigurationRestCartItemsAttributesMapperPlugin.php
    fixed doc-block

--- src/Spryker/Glue/ProductConfigurationsRestApi/Plugin/CartsRestApi/ProductConfigurationRestCartItemsAttributesMapperPlugin.php
+++ src/Spryker/Glue/ProductConfigurationsRestApi/Plugin/CartsRestApi/ProductConfigurationRestCartItemsAttributesMapperPlugin.php
@@ -20,7 +20,7 @@ class ProductConfigurationRestCartItemsAttributesMapperPlugin extends AbstractPl
     /**
      * {@inheritDoc}
      * - Maps `ItemTransfer` product configuration to `RestItemsAttributesTransfer`.
-     * - Executes `RestCartItemProductConfigurationMapperPluginInterface` plugin stack.
+     * - Executes `\Spryker\Glue\ProductConfigurationsRestApiExtension\Dependency\Plugin\RestProductConfigurationPriceMapperPluginInterface` plugin stack.
      *
      * @api
      *
